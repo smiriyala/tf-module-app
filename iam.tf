4c8e5c9691ca5473d549beee9b00a229d8d241e8
subbum <subbu@gmail.com>
2023-06-28 20:18:25 +0100

    IAM policy added to access all docdb parameters of aws_ssm

--- iam.tf
+++ iam.tf
@@ -17,7 +17,10 @@ resource "aws_iam_policy" "main" {
                 "ssm:GetParameters",
                 "ssm:GetParameter"
             ],
-            "Resource": "arn:aws:ssm:us-test-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*"
+            "Resource": [
+                "arn:aws:ssm:us-test-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*",
+                "arn:aws:ssm:us-test-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.docdb.*"
+            ]
         },
         {
             "Sid": "VisualEditor1",
